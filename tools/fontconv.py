d2285081c16c241107c4b499c89098c4e3d54b4c
Ben Russell <thematrixeatsyou@gmail.com>
2019-06-01 10:39:07 +1200

    fontconv: Support non-alpha + non-RGB fonts; Python 3 support

--- tools/fontconv.py
+++ tools/fontconv.py
@@ -3,31 +3,41 @@
 import sys, struct
 from PIL import Image, ImageOps, ImageColor
 
-im = Image.open(sys.argv[1])
+im = Image.open(sys.argv[1]).convert("RGBA")
 fp = open(sys.argv[2], "wb")
 
 def find_width(fch, offx, offy):
 	if fch == 32:
 		return 6
-	for ix in xrange(7, 1, -1):
-		for iy in xrange(8):
+	for ix in range(7, 1, -1):
+		for iy in range(8):
 			if im.getpixel((offx+ix, offy+iy))[3] > 0:
 				return ix+1
 	return 1
 
-for fch in xrange(0, 128, 2):
+for fch in range(0, 128, 2):
 	offx = (fch & 15) * 8
 	offy = (fch >> 4) * 8
 	fp.write(struct.pack("<B", find_width(fch, offx, offy) | (find_width(fch + 1, offx + 8, offy) << 4)))
 
-for iy in xrange(64):
-	for ix in xrange(0, 128, 2):
+has_alpha = False
+for iy in range(64):
+	for ix in range(128):
+		impc = im.getpixel((ix, iy))
+		if impc[3] == 0:
+			has_alpha = True
+			break
+
+alpha_idx = (3 if has_alpha else 0)
+
+for iy in range(64):
+	for ix in range(0, 128, 2):
 		v = 0
 		impc = im.getpixel((ix, iy))
-		if (impc[3] > 0):
+		if (impc[alpha_idx] > 0):
 			v |= 1
 		impc = im.getpixel((ix+1, iy))
-		if (impc[3] > 0):
+		if (impc[alpha_idx] > 0):
 			v |= 16
 		fp.write(struct.pack("<B", v))
 
