3d3b968f2a804ce6efb4623021d58b35b3334f49
asie <kontakt@asie.pl>
2019-06-01 23:27:31 +0200

    mkatlas: add option for loading animated lava/water sheets, fix non-transparent PNGs

--- tools/mkatlas.py
+++ tools/mkatlas.py
@@ -2,8 +2,13 @@ import sys, struct
 from PIL import Image, ImageOps, ImageColor
 
 im = Image.open(sys.argv[1])
+imwater = None
+imlava = None
+if len(sys.argv) >= 5:
+	imwater = Image.open(sys.argv[3])
+	imlava = Image.open(sys.argv[4])
 fp = open(sys.argv[2], "wb")
-clut = []
+clut = [None] * 256
 imgdata = [None] * (256*256)
 imgwidth = 256
 
@@ -13,11 +18,13 @@ def draw_4bit(im, ix, iy, iw, ih, tx, ty):
 	imgpalr = imgp.getpalette()
 	palette = [None] * 16
 	for col in range(16):
-		palette[col] = [imgpalr[col * 3], imgpalr[col * 3 + 1], imgpalr[col * 3 + 2], 0]
+		palette[col] = [imgpalr[col * 3], imgpalr[col * 3 + 1], imgpalr[col * 3 + 2], 255]
 	for iry in range(ih):
 		for irx in range(iw):
 			impc = imgp.getpixel((irx, iry))
-			palette[impc][3] = img.getpixel((irx, iry))[3]
+			impp = img.getpixel((irx, iry))
+			if len(impp) > 3:
+				palette[impc][3] = impp[3]
 			imgdata[(ty+iry)*imgwidth + tx+irx] = impc
 	return palette
 
@@ -32,10 +39,21 @@ def write_palette(fp, palette):
 			v = 0x8000|(r<<0)|(g<<5)|(b<<10)
 		fp.write(struct.pack("<H", v))
 
+def add_texture(im, ix, iy, i):
+	tx = (i & 15) << 4
+	ty = (i & 240)
+	clut[i] = draw_4bit(im, ix, iy, 16, 16, tx, ty)
+
 for i in range(256):
-	ix = (i & 15) << 4
-	iy = (i & 240)
-	clut.append(draw_4bit(im, ix, iy, 16, 16, ix, iy))
+	tx = (i & 15) << 4
+	ty = (i & 240)
+	add_texture(im, tx, ty, i)
+
+if imlava != None:
+	for i in range(20):
+		add_texture(imlava, 0, i*16, 80+i)
+	for i in range(32):
+		add_texture(imwater, 0, i*16, 100+i)
 
 for iy in range(256):
 	for ix in range(0, 256, 2):
